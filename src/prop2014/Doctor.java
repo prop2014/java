dcf8ad76dd32040afa47ee2365557e74c31b4219
alexmorral <alexmorral@gmail.com>
2014-10-17 15:36:44 +0200

    Modificado Doctors.java para que Hospital.java no de problemas. Añadidas funciones: inicializar, imprimir y geid

--- src/prop2014/Doctor.java
+++ src/prop2014/Doctor.java
@@ -2,4 +2,18 @@ package prop2014;
 
 public class Doctor {
 
+	
+	public void inicializarDoctor(int i) {}
+	
+	public void imprimirDoctor(Doctor d) {}
+	
+	public void getid(int i) {}
+	
+	
+	
+	
+	
+	
+	
+	
 }
