1692413fa7138db72b8fdcf60f3e737af334a4ed
ryblade_acer <alexmorral@gmail.com>
2014-10-27 13:18:44 +0100

    Añadida clase Dijkstra

---
+++ src/Domain/CtrlDijkstra.java
@@ -0,0 +1,14 @@
+package prop2014; import java.util.*;
+
+public class ffdijkstra extends ??? {
+
+/* pre G es un grafo dirigido no vacío con pesos en las aristas. initVertex y endVertex son dos vértices de G. */
+
+/* post Devuelve TRUE si existe un camino entre initVertex y endVertex. En este caso, Path contiene dicho camino que será el de coste mínimo. En caso contrario, devuelve FALSE. Weights contiene los costes desde initVertex hasta el resto de vértices. Si no hay camino a un vértice desde initVertex, la distancia será "infinito". */
+
+private boolean getPath(Graph<T> G, int initVertex, int endVertex, double[] Weights, int[] Path){return true;} 
+
+
+
+
+}
