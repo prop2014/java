7dae9a92d7ad46de1c3e80cf37dd6aa01282433a
Axel <axel_13th@hotmail>
2014-10-31 15:02:18 +0100

    NOT_Turno update

--- src/Model/NOT_Turno.java
+++ src/Model/NOT_Turno.java
@@ -1,20 +1,37 @@
 //Autor: Axel's Copyright 
 package Model;
 import java.util.*;
+import Model.Restriccion;
 
-public class NOT_Turno {
+public class NOT_Turno extends Restriccion {
 
 	/* Atributos */
 	private String TipoTurno;
 
 
 	/* Constructora */
-
-	public NOT_Turno() {}
-
 	
+	public NOT_Turno(int id, String T_Turno) {
+		id_Restriccion = id;
+		Tipo = "NOT_Turno"; 
+		TipoTurno = T_Turno; 
+		
+	}
 
 	/* Metodos públicos */
+	
+//--> Consultoras
+	
+	public int getId() {
+	    return id_Restriccion ;
+	}
+	public String getTipoTurno(){
+		return TipoTurno;
+	}
+	public String getTipo(){
+		return Tipo;
+	}
+	
 
 	
 	
