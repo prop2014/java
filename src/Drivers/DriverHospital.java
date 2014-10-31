92dd88d3208fe609f0d205029f24fdc04e478aa6
snowoner <oscar.urgelles@gmail.com>
2014-10-31 14:26:15 +0100

    driv

--- src/Drivers/DriverHospital.java
+++ src/Drivers/DriverHospital.java
@@ -1,13 +1,34 @@
 package Drivers;
+import java.util.Scanner;
+import Model.Hospital;
 
 public class DriverHospital {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner teclado;
+		Hospital hosp = new Hospital();
+		int id;
+		String n;
+		double fm;
+		double ft;
+		double fn;
+		
+		teclado = new Scanner(System.in);
+		System.out.print("Ingrese el codigo del hospital: ");
+		id = teclado.nextInt();
+		System.out.print("Ingrese el nombre del hospital: ");
+		n = teclado.next();
+		System.out.print("Ingrese el factorM: ");
+		fm = teclado.nextDouble();
+		System.out.print("Ingrese el factorT: ");
+		ft = teclado.nextDouble();
+		System.out.print("Ingrese el factorN: ");
+		fn = teclado.nextDouble();
+		
+		
+		Hospital husp = new Hospital(id,n,fm,ft,fn);
+	System.out.printf("ID Hospital %d, Nom Hospital: %s\n", husp.getId(), husp.getNombre());
+	System.out.printf("ID 1 %d", hosp.getId());
 	}
-
 }
