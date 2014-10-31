9b6da7ae3ba379a2016de292e46b02663e31155b
Sergi <sergiorra17@gmail.com>
2014-10-31 14:59:46 +0100

    Driver Doctor

--- src/Drivers/DriverDoctor.java
+++ src/Drivers/DriverDoctor.java
@@ -1,15 +1,32 @@
 package Drivers;
 import Model.Doctor;
+import Model.Hospital;
+
+import java.util.*;
 
 public class DriverDoctor {
 	
 	
 	public static void main(String args[]) {
-		Doctor doc = new Doctor();
-		doc.setId(100);
-		int idDoc = doc.getId();
-		System.out.printf("ID Doctor %d", idDoc);
-	}
-
-	
+		Scanner teclado;
+		int id, numMax;
+		String nombre;
+		Double sueldo;		
+		teclado = new Scanner(System.in);
+		
+		System.out.print("Ingrese el identificador del doctor: ");
+		id = teclado.nextInt();
+		System.out.print("Ingrese el nombre del doctor: ");
+		nombre = teclado.next();
+		System.out.print("Ingrese el numero maximo de turnos del doctor: ");
+		numMax = teclado.nextInt();	
+		System.out.print("Ingrese el sueldo del doctor: ");
+		sueldo = teclado.nextDouble();
+		
+		Doctor doc = new Doctor(id,nombre, numMax, sueldo);
+		System.out.printf("ID Doctor: %d\n", doc.getId());
+		System.out.printf("Nombre Doctor: %s\n", doc.getNombre());
+		System.out.printf("NumMaxTurnos Doctor: %d\n", doc.getNumMaxTurnos());
+		System.out.printf("Sueldo Doctor: %f\n", doc.getSueldoTurno());
+		}	
 }
