4cc765afced1073e3a7b5ca7be1155461dd327ee
Sergi <sergiorra17@gmail.com>
2014-10-31 13:23:02 +0100

    Actualitzada classe

--- src/Model/Doctor.java
+++ src/Model/Doctor.java
@@ -1,9 +1,14 @@
+//Autor Sergi Orra
 package Model;
 
 public class Doctor {
 
 	/* Atributos */
-		private int id;
+	private int id_doctor;
+	private String nombre;
+	private int num_max_turnos;
+	private double SueldoTurno;
+	private Scanner teclado;
 
 	
 	
@@ -19,12 +24,37 @@ public class Doctor {
 		
 		public void imprimirDoctor(Doctor d) {}
 		
-		public int getid() {
-			return id;
+		public int getId() {
+			return id_doctor;
 		}
-		
-		public void setid(int i){
-			this.id = i;
+
+		    
+		public String getNombre() {
+		    return nombre;
+		}
+		    
+		public int getNum_max_turnos() {
+			return num_max_turnos;
+		}
+		    
+		public double getSueldoTurno() {
+		    return SueldoTurno;
+		}
+		    
+	    public void setId(int id) {
+		    this.id_doctor = id;
+		}
+		    
+		public void setNombre(String nombre) {
+		    this.nombre = nombre;
+		}
+		    
+		public void setNum_max_turnos(int num) {
+		    this.num_max_turnos= num;
+		}
+		    
+		public void setSueldoTurno(float sueldo) {
+		    this.SueldoTurno = sueldo;
 		}
 	
 }
