6cba1a4b1d20142224c7a160c713fe8ef9b58b20
snowoner <oscar.urgelles@gmail.com>
2014-10-31 15:00:34 +0100

    fraseperl'alexmolpesau

--- src/Drivers/DriverHospital.java
+++ src/Drivers/DriverHospital.java
@@ -14,7 +14,6 @@ public class DriverHospital {
 		double ft;
 		double fn;
 		
-		
 		teclado = new Scanner(System.in);
 		System.out.print("Ingrese el codigo del hospital: ");
 		id = teclado.nextInt();
@@ -27,9 +26,25 @@ public class DriverHospital {
 		System.out.print("Ingrese el factorNoche: ");
 		fn = teclado.nextDouble();
 		
-		
 		Hospital husp = new Hospital(id,n,fm,ft,fn);
-	System.out.printf("ID Hospital %d, Nom Hospital: %s\n", husp.getId(), husp.getNombre());
-	System.out.printf("ID %d", hosp.getId());
+		System.out.printf("ID Hospital %d, Nom Hospital: %s\n", husp.getId(), husp.getNombre());
+	
+		System.out.print("cambiali el nom: ");
+		n = teclado.next();
+		husp.setNombre(n);
+		System.out.printf("ID Hospital %d, Nom Hospital: %s\n FM: %f, FT: %f, FN: %f\n", husp.getId(), husp.getNombre(), husp.getFactorM(),
+			husp.getFactorT(), husp.getFactorN());
+		System.out.print("cambiale factorMañana: ");
+		fm = teclado.nextDouble();
+		husp.setFactorM(fm);
+		System.out.print("Ingrese el factorTarde: ");
+		ft = teclado.nextDouble();
+		husp.setFactorT(ft);
+		System.out.print("Ingrese el factorNoche: ");
+		fn = teclado.nextDouble();
+		husp.setFactorN(fn);
+		
+		System.out.printf("ID Hospital %d, Nom Hospital: %s\n FM: %f, FT: %f, FN: %f\n", husp.getId(), husp.getNombre(), husp.getFactorM(),
+				husp.getFactorT(), husp.getFactorN());
 	}
 }
