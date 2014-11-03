27b8d367b65c7c621576c1f3221e8da1bfd21ad1
felixr <felixramosmusic@yahoo.es>
2014-11-03 12:39:25 +0100

    incompleta (he de aclarar dudas)

--- src/Model/Turno.java
+++ src/Model/Turno.java
@@ -1,17 +1,67 @@
 package Model;
 
+/**
+ *
+ * @author Felix Fernando Ramos Velázquez
+ */
 public class Turno {
 
 	/* Atributos */
+  private Date fecha;
+  private TipoTurno turno;
+  private String especial;
+  private int numDoctores;
 
-	/* Constructora */
+	/* Constructoras */
 
-	public Turno() {}
+  /**
+   *
+   * Constructora por defecto
+   */
+	public Turno() {
+    fecha = null;
+    turno = null;
+    especial = null;
+    numDoctores = 0;
+  }
 
 	
 
 	/* Metodos públicos */
 
+	/* Modificadoras */
+  
+  public void setFecha(Date d){
+    fecha = d;
+  }
+  
+  public void setTipoTurno(TipoTurno tt){
+    turno = tt;
+  }
+  
+  public void setEspecial(String esp){
+    especial = esp;
+  }
+  
+  public void setNumDoctores(int n){
+    numDoctores = n;
+  }
+  
+  /* Consultoras */
+  
+  public Date getFecha(){
+    return fecha;
+  }
 	
-	
+  public String getTipoTurno(){
+    return turno;
+  }
+  
+  public String getEspecial(){
+    return especial;
+  }
+  
+  public int getNumDoctores(){
+    return numDoctores;
+  }
 }
