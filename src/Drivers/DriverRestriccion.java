52f289db7c3914fa48c96a35b477ca1f642d903d
Axel <axel_13th@hotmail>
2014-11-06 17:52:07 +0100

    primera implementacion DRivers Restricciones v2

--- src/Drivers/DriverRestriccion.java
+++ src/Drivers/DriverRestriccion.java
@@ -2,6 +2,7 @@ package Drivers;
 import java.util.*;
 
 import Model.Doctor;
+import Model.Hospital;
 import Model.Restriccion;
 import Model.MAX_Dias_Rango;
 import Model.MAX_Turnos_Consecutivos;
@@ -15,6 +16,58 @@ import Model.XOR;
 
 
 public class DriverRestriccion {
+	
+	
+	static void MostrarRestriccion(Restriccion RES){
+		
+		Scanner teclado;
+		teclado = new Scanner(System.in); 
+		
+		switch(RES.getTipo()){
+		case "XOR" :{//XOR
+			
+			break;
+		}
+		
+		case "NOT_Dia_Mes" :{ //NOT_Dia_Mes
+			System.out.printf("antes del print dentro del case");
+			//System.out.printf("ID de Restriccion %d Restriccion tipo:  %s, Dia restringido %d\n", RES.getId(), RES.getTipo(), RES.getDia_Mes());
+			//System.out.printf("ID de Restriccion %d Restriccion tipo:  %s \n", RES.getId(), RES.getTipo(), RES.getDiaMes());
+			break;
+		}
+		case "NOT_Dia_Semana" :{ //NOT_Dia_Semana
+			
+			break;
+		}
+		case "NOT_Especial" :{// NOT_Especial
+			
+			break;
+		}
+		case "NOT_Fecha" :{ //NOT_Fecha
+			
+			break;
+		}
+		case "NOT_Turno" :{ //NOT_Turno
+			
+			break;
+		}
+		case "MAX_Turnos_por_Dia" :{ //MAX_Turnos_por_Dia
+			
+			break;
+		}
+		case "MAX_Turnos_Consecutivos"  :{ //MAX_Turnos_Consecutivos
+			
+			break;
+		}
+		
+		case "MAX_Dias_Rango"  :{ //MAX_Dias_Rango
+			
+			break;
+		}
+		
+		}
+		
+	}
 	
 
 	public static void main(String[] args) {
@@ -30,25 +83,28 @@ public class DriverRestriccion {
 			
 			System.out.print("¿Que desea hacer?\n\n");
 			
-			System.out.print("1: Crear Restriccion tipo XOR\n");
+			System.out.print("NOP 1: Crear Restriccion tipo XOR\n");
 			System.out.print("2: Crear Restriccion tipo NOT_Dia_Mes\n");
 			System.out.print("3: Crear Restriccion tipo NOT_Dia_Semana\n");
 			System.out.print("4: Crear Restriccion tipo NOT_Especial\n");
-			System.out.print("5: Crear Restriccion tipo NOT_Fecha\n");
+			System.out.print("NOP 5: Crear Restriccion tipo NOT_Fecha\n");
 			System.out.print("6: Crear Restriccion tipo NOT_Turno\n");
 			System.out.print("7: Crear Restriccion tipo MAX_Turnos_por_Dia\n");
 			System.out.print("8: Crear Restriccion tipo MAX_Turnos_Consecutivos\n");
-			System.out.print("9: Crear Restriccion tipo MAX_Dias_Rango\n");
+			System.out.print("NOP 9: Crear Restriccion tipo MAX_Dias_Rango\n");
 			System.out.print("10: Ver Restriccion ya creada\n");
 		
+			
 			opcion = teclado.nextInt(); //leemos opcion
 			
 			
 			
+			Restriccion RES = new Restriccion(); 
+			
 			
 			switch(opcion){
 				case 1:{ //XOR
-					
+					System.out.print("ESTA RESTRICCION NO ESTA OPERATIVA");
 					break;
 				}
 				case 2: { //NOT_Dia_Mes
@@ -56,7 +112,8 @@ public class DriverRestriccion {
 					int Dia;
 					System.out.print("Introduce el dia del mes(1-30) que no quieras trabajar\n");
 					Dia = teclado.nextInt();
-					NOT_Dia_Mes res = new NOT_Dia_Mes(id, Dia);
+					RES = new NOT_Dia_Mes(id, Dia);
+					
 					
 					break;
 				}
@@ -65,25 +122,26 @@ public class DriverRestriccion {
 					String Dia;
 					System.out.print("Introduce el dia de la semana(Lunes-Domingo) que no quieras trabajar\n");
 					Dia = teclado.next();
-					NOT_Dia_Semana res = new NOT_Dia_Semana(id, Dia);
+					RES = new NOT_Dia_Semana(id, Dia);
 					
 					break;
 				}
-				case 4:{// NOT_Especial
+				case 4:{ //NOT_Especial
 					++id;
 					String Dia;
 					System.out.print("Introduce el dia Especial(Navidad,SantGervasi,...) que no quieras trabajar\n");
 					Dia = teclado.next();
-					NOT_Especial res = new NOT_Especial( id, Dia);
+					RES = new NOT_Especial( id, Dia);
 					
 					break;
 				}
 				case 5:{ //NOT_Fecha
+					System.out.print("ESTA RESTRICCION NO ESTA OPERATIVA");
 					/*++id;
 					Date Dia;
 					System.out.print("Introduce la fecha exacta que no quieras trabajar\n");
 			-------->Dia = teclado.next();
-					NOT_Especial res = new NOT_Especial( id, Dia); */
+					RES = new NOT_Especial( id, Dia); */
 				}
 					
 					break;
@@ -93,7 +151,7 @@ public class DriverRestriccion {
 					String Turno;
 					System.out.print("Introduce el Turno (Mañana/Tarde/Noche) que no quieras trabajar\n");
 					Turno = teclado.next();
-					NOT_Especial res = new NOT_Especial( id, Turno);
+					RES = new NOT_Especial( id, Turno);
 					
 					break;
 				}
@@ -102,7 +160,7 @@ public class DriverRestriccion {
 					int Dias;
 					System.out.print("Introduce el maximo de turnos que quieras trabajar por dia (1 o 2)\n");
 					Dias = teclado.nextInt();
-					MAX_Turnos_por_Dia res = new MAX_Turnos_por_Dia(id, Dias);
+					RES = new MAX_Turnos_por_Dia(id, Dias);
 					break;
 				}
 				case 8:{//MAX_Turnos_Consecutivos
@@ -110,6 +168,7 @@ public class DriverRestriccion {
 					break;
 				}
 				case 9:{ //MAX_Dias_Rango
+					System.out.print("ESTA RESTRICCION NO ESTA OPERATIVA");
 					/*++id;
 					int Dias;
 					Date Dia;
@@ -119,13 +178,16 @@ public class DriverRestriccion {
 	 -------------->Dia = teclado.();
 					System.out.print("Introduce el maximo de turnos que quieras trabajar en ese rango de dias\n");
 					Dias = teclado.nextInt();
-					MAX_Dias_Rango res = new MAX_Dias_Rango(id, Dias); */
+					RES = new MAX_Dias_Rango(id, Dias); */
 					break;
 					
 				}
 				case 10:{
 					
-					if(id != -1) System.out.print("FALTA PROGRMAR LAS SALIDAS "); //MostrarRestriccion();
+					if(id != -1) {
+						
+						MostrarRestriccion(RES);
+					}
 					else System.out.print("Aun No hay ninguna Restriccion!\n\n");
 					
 					break;
@@ -141,6 +203,7 @@ public class DriverRestriccion {
 			s = teclado.next();
 			// Si seleccionamos "No" o sus variantes saldremos del bucle
 			if(s.equals("No") || s.equals("no") || s.equals("N") || s.equals("NO") || s.equals("n")) opcion = 0; 
+			
 			
 		}
 		System.out.print("PROGRAM EXIT");
