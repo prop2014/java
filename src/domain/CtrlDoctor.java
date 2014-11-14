02431b3b8f5d91bacc8902edde0e41f0fdd75484
alexmorral <alexmorral@gmail.com>
2014-11-14 14:55:48 +0100

    Split de CtrlDominio en CtrlHospital y CtrlDoctor

---
+++ src/domain/CtrlDoctor.java
@@ -0,0 +1,30 @@
+package domain;
+
+import model.Doctor;
+import model.Restriccion;
+
+public class CtrlDoctor {
+	/** Atributos */
+	//private Doctor??
+	
+	
+	public void anadirRestriccionTipoX() {
+		//updateDoctorData()?
+	}
+	
+	public void anadirRestriccionTipoY() {
+		//updateDoctorData()?
+	}
+	
+	public void anadirRestriccionTipoZ() {
+		//updateDoctorData()?
+	}
+	
+	public void anadirRestriccionTipoK() {
+		//updateDoctorData()?
+	}
+	
+	public void eliminarRestriccion(int id) {
+		//updateDoctorData()?
+	}
+}
