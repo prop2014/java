74567097aed3e74b612a8d45d21136b544a1abaf
Axel <axel_13th@hotmail>
2014-11-28 14:19:25 +0100

    vistaAlertHospital y VistaGestion

--- src/views/VistaAlertHospital.java
+++ src/views/VistaAlertHospital.java
@@ -1,35 +1,134 @@
 package views;
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.border.EmptyBorder;
 
-import java.awt.Color;
+/**
+ * Vista principal de la gestion de un hospital
+ * @author Axel Pelaez
+ */
+	
+	public class VistaAlertHospital {
+		
+		//Componentes interficie
+		private JFrame frameView = new JFrame("WARNING");
+		private JPanel panelContents = new JPanel();
+		
+		private JPanel panelCenterButtons = new JPanel();
+		
+		
+		
+		
+		//CENTER
+		
+		private JLabel labelPanel1 = new JLabel("<html>Ya existe un Calendario para: <br/> ''NOMBRE DEL HOSPITAL");
+		private JLabel labelPanel2 = new JLabel("<html><CENTER>¿Que desea hacer?</CENTER>"); 
+		private JButton buttonCrearCal = new JButton("<html><CENTER>Crear nuevo <br/> Calendario</CENTER>");
+		private JButton buttonImportCal = new JButton("<html><CENTER>Importar nuevo <br/> Calendario</CENTER>");
+		private JButton buttonCancel = new JButton("<html>Cancelar");
+		
+		
+		
+		
+		//METODOS PRIVADOS
+		
+	
+		
+		private void inicializar_frameView() {
+			// Tamanyo
+		    frameView.setMinimumSize(new Dimension(700,400));
+		    frameView.setPreferredSize(frameView.getMinimumSize());
+		    frameView.setResizable(false);
+		    // Posicion y operaciones por defecto
+		    frameView.setLocationRelativeTo(null);
+		    frameView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		    JPanel contentPane = (JPanel) frameView.getContentPane();
+		    contentPane.add(panelContents);
+		}
+		
+	
+		
+		private void inicializar_panelContents() {
+			panelContents.setLayout(null);
+			panelCenterButtons.setBounds(0, 0, 700, 372);
+			// Componentes
+			
+			panelContents.add(panelCenterButtons);
+						
+		}
+		
+		
+		
+	
+		
 
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+	
+		
+		private void inicializar_panelCenterButtons() {
+			
+			////// START: GESTIONADO POR EL BUILDER NO TOCAR
+			panelCenterButtons.setBorder(new EmptyBorder(70, 20, 20, 0));
+		
+			buttonCrearCal.setBounds(50, 271, 177, 50);;
+			
+			buttonImportCal.setBounds(251, 271, 177, 50);
+			
+			buttonCancel.setBounds(462, 271, 177, 50);
+			
+			labelPanel1.setBounds(78, 47, 226, 44);
+			labelPanel2.setBounds(267, 172, 191, 38);
+			
+			panelCenterButtons.setLayout(null);
+			/// END: GESTIONADO POR EL BUILDER NO TOCAR
+			
+			// Components
+			
+			panelCenterButtons.add(labelPanel1);
+			panelCenterButtons.add(labelPanel2);
+			panelCenterButtons.add(buttonImportCal);
+			panelCenterButtons.add(buttonCrearCal);
+			panelCenterButtons.add(buttonCancel);
+			
+		}
+		
+		
+		private void inicializarComponents() {
+		    inicializar_frameView();
+		    inicializar_panelContents();
+		    inicializar_panelCenterButtons();
+		    assignar_listenersComponents();
+		  }
 
-public class VistaAlertHospital {
 
-	  /**
-	   * @wbp.parser.entryPoint
-	   */
-	  public void hacerVisible() {
-	    String strTitulo = "LEEME";
-	    String[] strBotones = {"Ok"};
-	    String strTexto =
-	      "Este ejemplo no pretende ser un modelo universal, sino un posible " + "\n" + 
-	      "punto de partida para la construcción de una interficie en Java. "  + "\n" +
-	      "\n" + 
-	      "Las interficies en Java se pueden hacer de muchas maneras, y ésta " + "\n" + 
-	      "es una de ellas, pero en ningún momento se sugiere que haya que "+ "\n" + 
-	      "hacerlo así o que ésta sea la mejor manera de hacerlo.\n";
-	    JOptionPane optionPane =
-	      new JOptionPane(strTexto,JOptionPane.INFORMATION_MESSAGE);
-	    optionPane.setOptions(strBotones);
-	    optionPane.setBackground(Color.orange);
-	    JDialog dialogOptionPane = optionPane.createDialog(new JFrame(),strTitulo);
-	    dialogOptionPane.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-	    dialogOptionPane.pack();
-	    dialogOptionPane.setVisible(true);
-	  }
+		private void assignar_listenersComponents() {
+		}
+		
+		
+		//METODOS PUBLICOS
+		/**
+		 * @wbp.parser.entryPoint
+		 */
+		public VistaAlertHospital() {
+			inicializarComponents();
+		}
+	
+		public void showView() {
+			frameView.setVisible(true);
+		}
 
-}
+		public void enableView() {
+			frameView.setEnabled(true);
+		}
+
+		public void disableView() {
+			frameView.setEnabled(false);
+		}
+		
+		public static void main(String[] args) {
+			VistaGestion v = new VistaGestion();
+			v.showView();
+		}
+	}
+
 
--- src/views/VistaGestion.java
+++ src/views/VistaGestion.java
@@ -1,133 +1,79 @@
 package views;
 import javax.swing.*;
-
 import java.awt.*;
 import javax.swing.border.EmptyBorder;
-
-
 /**
  * Vista principal de la gestion de un hospital
  * @author Axel Pelaez
  */
-	
-	public class VistaGestion {
-		
-		//Componentes interficie
-		private JFrame frameView = new JFrame("Programador de Guardias");
-		private JPanel panelContents = new JPanel();
-		
-		private JPanel panelCenterButtons = new JPanel();
-		
-		
-		
-		
-		//CENTER
-		
-		private JLabel labelPanel1 = new JLabel("<html><u>''NOMBRE DEL HOSPITAL''</u>");
-		private JButton buttonCal = new JButton("<html><CENTER>Gestion de <br/>Calendario</CENTER>");
-		private JButton buttonDoc = new JButton("<html><CENTER>Gestion de <br/>Doctores</CENTER>");
-		private JButton buttonSol = new JButton("<html><CENTER>Gestion de <br/>Solucion</CENTER>");
-		private JButton buttonVolver = new JButton("Volver");
-		
-		
-		
-		//METODOS PRIVADOS
-		
-	
-		
-		private void inicializar_frameView() {
-			// Tamanyo
-		    frameView.setMinimumSize(new Dimension(700,400));
-		    frameView.setPreferredSize(frameView.getMinimumSize());
-		    frameView.setResizable(false);
-		    // Posicion y operaciones por defecto
-		    frameView.setLocationRelativeTo(null);
-		    frameView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		    JPanel contentPane = (JPanel) frameView.getContentPane();
-		    contentPane.add(panelContents);
-		}
-		
-	
-		
-		private void inicializar_panelContents() {
-			panelContents.setLayout(null);
-			panelCenterButtons.setBounds(0, 0, 700, 372);
-			// Componentes
-			
-			panelContents.add(panelCenterButtons);
-						
-		}
-		
-		
-		
-	
-		
-
-	
-		
-		private void inicializar_panelCenterButtons() {
-			
-			////// START: GESTIONADO POR EL BUILDER NO TOCAR
-			panelCenterButtons.setBorder(new EmptyBorder(70, 20, 20, 0));
-		
-			buttonCal.setBounds(52, 134, 173, 119);;
-			
-			buttonSol.setBounds(469, 134, 173, 119);
-			
-			buttonDoc.setBounds(263, 134, 173, 119);
-			
-			buttonVolver.setBounds(52, 323, 157, 25);
-			
-			labelPanel1.setBounds(32, 12, 177, 15);
-			
-			panelCenterButtons.setLayout(null);
-			/// END: GESTIONADO POR EL BUILDER NO TOCAR
-			
-			// Components
-			
-			panelCenterButtons.add(labelPanel1);
-			panelCenterButtons.add(buttonVolver);
-			panelCenterButtons.add(buttonCal);
-			panelCenterButtons.add(buttonSol);
-			panelCenterButtons.add(buttonDoc);
-		}
-		
-		
-		private void inicializarComponents() {
-		    inicializar_frameView();
-		    inicializar_panelContents();
-		    inicializar_panelCenterButtons();
-		    assignar_listenersComponents();
-		  }
-
-
-		private void assignar_listenersComponents() {
-		}
-		
-		
-		//METODOS PUBLICOS
-		
-		public VistaGestion() {
-			inicializarComponents();
-		}
-		
-		public void showView() {
-			frameView.setVisible(true);
-		}
-
-		public void enableView() {
-			frameView.setEnabled(true);
-		}
-
-		public void disableView() {
-			frameView.setEnabled(false);
-		}
-		
-		public static void main(String[] args) {
-			VistaGestion v = new VistaGestion();
-			v.showView();
-		}
+public class VistaGestion {
+	//Componentes interficie
+	private JFrame frameView = new JFrame("Programador de Guardias");
+	private JPanel panelContents = new JPanel();
+	private JPanel panelCenterButtons = new JPanel();
+	//CENTER
+	private JLabel labelPanel1 = new JLabel("<html><u>''NOMBRE DEL HOSPITAL''</u>");
+	private JButton buttonCal = new JButton("<html><CENTER>Gestion de <br/>Calendario</CENTER>");
+	private JButton buttonDoc = new JButton("<html><CENTER>Gestion de <br/>Doctores</CENTER>");
+	private JButton buttonSol = new JButton("<html><CENTER>Gestion de <br/>Solucion</CENTER>");
+	private JButton buttonVolver = new JButton("Volver");
+	//METODOS PRIVADOS
+	private void inicializar_frameView() {
+		// Tamanyo
+		frameView.setMinimumSize(new Dimension(700,400));
+		frameView.setPreferredSize(frameView.getMinimumSize());
+		frameView.setResizable(false);
+		// Posicion y operaciones por defecto
+		frameView.setLocationRelativeTo(null);
+		frameView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel contentPane = (JPanel) frameView.getContentPane();
+		contentPane.add(panelContents);
 	}
-
-
+	private void inicializar_panelContents() {
+		panelContents.setLayout(new BorderLayout());
+		// Componentes
+		panelContents.add(panelCenterButtons, BorderLayout.CENTER);
+	}
+	private void inicializar_panelCenterButtons() {
+		////// START: GESTIONADO POR EL BUILDER NO TOCAR
+		panelCenterButtons.setBorder(new EmptyBorder(70, 20, 20, 0));
+		buttonCal.setBounds(49, 134, 173, 119);
+		buttonSol.setBounds(467, 132, 181, 122);
+		buttonDoc.setBounds(263, 134, 173, 119);
+		buttonVolver.setBounds(40, 324, 157, 25);
+		labelPanel1.setBounds(32, 12, 177, 15);
+		panelCenterButtons.setLayout(null);
+		/// END: GESTIONADO POR EL BUILDER NO TOCAR
+		// Components
+		panelCenterButtons.add(labelPanel1);
+		panelCenterButtons.add(buttonVolver);
+		panelCenterButtons.add(buttonCal);
+		panelCenterButtons.add(buttonSol);
+		panelCenterButtons.add(buttonDoc);
+	}
+	private void inicializarComponents() {
+		inicializar_frameView();
+		inicializar_panelContents();
+		inicializar_panelCenterButtons();
+		assignar_listenersComponents();
+	}
+	private void assignar_listenersComponents() {
+	}
+	//METODOS PUBLICOS
+	public VistaGestion() {
+		inicializarComponents();
+	}
+	public void showView() {
+		frameView.setVisible(true);
+	}
+	public void enableView() {
+		frameView.setEnabled(true);
+	}
+	public void disableView() {
+		frameView.setEnabled(false);
+	}
+	public static void main(String[] args) {
+		VistaGestion v = new VistaGestion();
+		v.showView();
+	}
+}
